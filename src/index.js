cb262f76a93d36323cd756fc5909414531863b1d
ejangi <james@ejangi.com>
2019-11-02 20:27:57 +1000

    Added Bootstrap, Router and two simple routes.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 import App from './components/App.vue'
+import Home from './components/Home.vue'
+import Tasks from './components/Tasks.vue'
+import '@/sass/main.scss'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/tasks', component: Tasks }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
