66dce8cc6c2ca193791026bc4a50f8b2e301cba1
ejangi <james@ejangi.com>
2019-11-03 10:04:47 +1000

    Added some basic layout adjustments

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>Hello world</h1>
+    <b-container class="mt-3">
+        <b-jumbotron header="Welcome" lead="The ultimate TODO app">
+                <p>Get started by creating your very first todo!</p>
+            <b-button variant="primary" to="/tasks">Get Started</b-button>
+        </b-jumbotron>
+    </b-container>
 </template>
 
 <script>
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
+    <b-container class="mt-3">
         <h1>Tasks</h1>
-    </div>
+
+        <b-card-group columns class="d-flex flex-column">
+            <b-card title="Default">
+                <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+                <b-button to="/tasks/1/complete" variant="primary">Complete</b-button>
+            </b-card>
+
+            <b-card title="Default">
+                <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+                <b-button to="/tasks/2/complete" variant="primary">Complete</b-button>
+            </b-card>
+        </b-card-group>
+    </b-container>
 </template>
 
 <script>
